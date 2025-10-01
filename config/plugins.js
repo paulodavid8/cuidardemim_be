d386e338c2483b175d455e661da45879b987ab38
PauloDavidWebSolutions <paulodaviddias8@gmail.com>
2025-10-01 23:33:40 +0100

    stmp emails

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,22 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    // ...
+    email: {
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: env('SMTP_HOST', 'smtp-relay.brevo.com'),
+          port: env('SMTP_PORT', 587),
+          auth: {
+            user: env('SMTP_USERNAME'),
+            pass: env('SMTP_PASSWORD'),
+          },
+          secure: false,
+        },
+        settings: {
+            defaultFrom: env('EMAIL_FROM'),
+            defaultReplyTo: env('EMAIL_FROM'),
+        },
+      },
+    },
+    // ...
+  });
