64fcf20aa5b03301492b0f5a18a3c71fee534d98
PauloDavidWebSolutions <paulodaviddias8@gmail.com>
2025-10-02 20:50:04 +0100

    nodemailer

--- config/plugins.js
+++ config/plugins.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
     // ...
     email: {
       config: {
-        provider: 'console',
+        provider: 'nodemailer',
         providerOptions: {
           host: env('EMAIL_HOST', 'smtp-relay.brevo.com'),
           port: env('EMAIL_PORT', 587),
